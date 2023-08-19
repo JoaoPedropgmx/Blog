cc75395f5874deb64ac985e42bd75b876b20eb13
joao <jpdspp@hotmail.com>
2023-08-19 16:38:15 -0300

    criando primeiro arquivo html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
-def home(request):
-    return HttpResponse('hello world')
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
