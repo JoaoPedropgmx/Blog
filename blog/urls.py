430963612967ee293f3e011100a8c213b711e104
joao <jpdspp@hotmail.com>
2023-08-20 19:50:17 -0300

    Finalizando Blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list, name='home'),
-    path('postagem/<int:pk>/', views.post_detalhes, name='post_detalhes')
+    path('postagem/<int:pk>/', views.post_detalhes, name='post_detalhes'),
+    path('postagem/new/', views.post_news, name='post_new'),
+    path('postagem/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('postagem/<int:pk>/delete/', views.post_delete, name='post_delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -10,3 +11,37 @@ def post_list(request):
 def post_detalhes(request, pk):
     posts = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalhes.html', {'posts':posts})
+
+# verifica o novo post, cria um novo objeto do tipo post e redireciona pra pagina do post
+def post_news(request):     
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.data_publicacao = timezone.now()
+            post.save()
+            return redirect('post_detalhes', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.data_publicacao = timezone.now()
+            post.save()
+            return redirect('post_detalhes', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'blog/post_list.html', {'posts': posts})
