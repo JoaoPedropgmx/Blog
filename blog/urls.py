2074f2810ef484dd47392d1941e71abf6da13bb5
joao <jpdspp@hotmail.com>
2023-08-20 15:09:05 -0300

    melhorando web

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list, name="home")
+    path('',views.post_list, name='home'),
+    path('postagem/<int:pk>/', views.post_detalhes, name='post_detalhes')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detalhes(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detalhes.html', {'posts':posts})
